d6d3cb6c992066ab697c543696729c582220808f
byunjiin <jibyun5808@naver.com>
2024-10-03 19:12:32 +0900

    ✨ : SearchBar 기본 구조

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,3 +1,27 @@
+import { useForm } from "react-hook-form";
+import InputBox from "./common/InputBox";
+import searchImg from "../assets/img/search.png";
+
+type SearchForm = {
+  search: string;
+};
+
 export default function SearchBar() {
-  return <div>d</div>;
+  const { register, handleSubmit } = useForm<SearchForm>({ mode: "onBlur" });
+  function onSubmit(data: SearchForm) {
+    console.log(data);
+  }
+  return (
+    <div>
+      <img src={searchImg} alt="검색 이미지" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <InputBox
+          inputBox="search"
+          type="text"
+          placeholder="Search.."
+          register={register("search")}
+        />
+      </form>
+    </div>
+  );
 }
