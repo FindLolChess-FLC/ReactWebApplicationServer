ce19adbd35925c0be6c7dedfcf04ce91d0efa676
byunjiin <jibyun5808@naver.com>
2024-10-23 19:34:51 +0900

    💄 : Button 공통 컴포넌트 스타일

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,6 +1,49 @@
-// import styled, { css } from "styled-components";
+import styled, { css } from "styled-components";
 import { ButtonProps } from "../../types/Button";
 
+const DefaultButton = styled.button`
+  width: 25.625rem;
+  height: 3.75rem;
+  border-radius: 8px;
+  background: #5144ed;
+  color: #fff;
+  font-size: 1rem;
+
+  /* 기본 버튼 hover 스타일 */
+  &:hover {
+    background: #6f63ff;
+  }
+
+  ${props =>
+    props.type === "submit" &&
+    css`
+      background: linear-gradient(90deg, #9600da 0%, #6200e0 50%, #33b1f5 100%);
+      overflow: hidden;
+
+      &:hover {
+        background: #fff; /* 배경을 흰색으로 변경 */
+        color: transparent; /* 글자를 투명하게 */
+        border: 4px solid transparent;
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+
+        background-image: linear-gradient(
+          90deg,
+          #9600da 0%,
+          #6200e0 50%,
+          #33b1f5 100%
+        );
+        border-image: linear-gradient(
+          90deg,
+          #9600da 0%,
+          #6200e0 50%,
+          #33b1f5 100%
+        );
+        border-image-slice: 1; /* 그라디언트를 테두리에 적용 */
+      }
+    `}
+`;
 export default function Button({
   type,
   button,
@@ -9,7 +52,7 @@ export default function Button({
   children,
 }: ButtonProps) {
   return (
-    <button
+    <DefaultButton
       type={type === "submit" ? "submit" : "button"}
       id={button}
       name={button}
@@ -17,6 +60,6 @@ export default function Button({
       onClick={onClick}
     >
       {children}
-    </button>
+    </DefaultButton>
   );
 }
