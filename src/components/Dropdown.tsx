087781907fbdb22fd837e19c69757794363af4cb
byunjiin <jibyun5808@naver.com>
2024-11-10 23:24:36 +0900

    ✨ : Token에 따른 닉네임

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { DropdownProps } from "../types/Dropdown";
+import { Api } from "../utils/apis/Api";
 
-export default function Dropdown({
-  handleLogout,
-}: {
-  handleLogout: () => void;
-}) {
+export default function Dropdown({ handleLogout }: DropdownProps) {
   // 상태 관리: 목록이 보이는지 여부를 결정하는 state
   const [isOpen, setIsOpen] = useState(false);
+  const [nickname, setNickname] = useState("");
+  useEffect(() => {
+    const nicknameAPI = async () => {
+      const nicknameData = await Api({
+        method: "GET",
+        lastUrl: "user/updateinfo/",
+      });
+      setNickname(nicknameData.nickname);
+    };
+    nicknameAPI();
+  }, []);
 
   // 토글 함수
   const handleDropdown = () => {
@@ -18,7 +27,7 @@ export default function Dropdown({
     <div>
       {/* 토글 버튼 */}
       <button type="button" onClick={handleDropdown}>
-        변지인님 환영합니다.
+        {`${nickname}님 환영합니다.`}
       </button>
       {/* 상태에 따라 목록 표시 */}
       {isOpen && (
