013a781af1b7e8155f2e8e6be499bc787818a941
byunjiin <jibyun5808@naver.com>
2024-11-17 20:22:53 +0900

    👷 : http build를 위한 수정

--- src/utils/setCookie.ts
+++ src/utils/setCookie.ts
@@ -2,5 +2,6 @@
 export default function setCookie(name: string, value: string, hours: number) {
   const date = new Date();
   date.setTime(date.getTime() + hours * 60 * 60 * 1000);
-  document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/; Secure;`;
+  // 지금은 http여서 이대로, nginx 후 https로 변경시 Secure 적용
+  document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/;`;
 }
