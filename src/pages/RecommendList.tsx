e96092a9dcf2f9f6f6fdbabee5032adcd0788d11
byunjiin <jibyun5808@naver.com>
2025-03-03 03:41:14 +0900

    ✨ : 무한 스크롤

--- src/pages/RecommendList.tsx
+++ src/pages/RecommendList.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import Header from "../components/containers/Header";
@@ -74,18 +74,56 @@ const SubTitle = styled.div`
 `;
 
 export default function RecommendList() {
-  const [metaData, setMetaData] = useState<ListForm[] | null>(null);
+  const [metaData, setMetaData] = useState<ListForm[]>([]);
+  // const [loading, setLoading] = useState(false);
+  const [nextUrl, setNextUrl] = useState<string | null>(null);
+  const observerTarget = useRef<HTMLDivElement | null>(null);
 
+  // api 가져오는 툴
+  const searchApi = async (query: string) => {
+    // if (loading) return; // 중복 호출 방지
+    // setLoading(true);
+    const response = await Api({
+      method: "GET",
+      lastUrl: `meta/metasearch/${query}`,
+    });
+    // 메타(이전값을 포함해서 새로운값도 불러옴)
+    setMetaData(prev =>
+      query === "" ? [...response.data] : [...prev, ...response.data],
+    );
+    // next_page_url에서 쿼리스트링만 추출
+    const nextPageQuery = response.next_page_url?.split("?")[1];
+    if (nextPageQuery) {
+      setNextUrl(`?${nextPageQuery}`); // 쿼리스트링만 저장
+    } else {
+      setNextUrl(null);
+    }
+    // setLoading(false);
+  };
+
+  // 추천 api 불러오기
   useEffect(() => {
-    const searchApi = async () => {
-      const response = await Api({
-        method: "GET",
-        lastUrl: "meta/metasearch/",
-      });
-      setMetaData(response.data);
-    };
-    searchApi();
+    searchApi("");
   }, []);
+
+  // 무한 스크롤에 맞춰서 api 불러오기
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // 관찰 중인 요소가 화면에 보일때 + 다음 url이 있을 때
+        if (entry.isIntersecting && nextUrl) {
+          searchApi(nextUrl);
+        }
+      }, // callback
+      { threshold: 0.3 }, // options
+    );
+    // 타겟 요소 감시 시작
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+    // 컴포넌트가 사라질 때 감시 종료
+    return () => observer.disconnect();
+  }, [nextUrl]);
 
   return (
     <Body>
@@ -105,7 +143,10 @@ export default function RecommendList() {
         <Contents>
           <SubTitle>추천 메타</SubTitle>
           {metaData && metaData.length > 0 ? (
-            <Meta metaData={metaData} />
+            <>
+              <Meta metaData={metaData} />
+              <div ref={observerTarget} /> {/* 감시를 위한 div */}
+            </>
           ) : (
             // 로딩
             <Skeleton
